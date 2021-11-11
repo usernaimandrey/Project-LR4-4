c7ccdabce76137f51fb159646c48d6588cea149c
usernaimandrey <anshlyapnikov@yandex.com>
2021-11-11 13:08:05 +0300

    Добавьте файлы проекта.

---
+++ Project-LR4-4/MyForm.cpp
@@ -0,0 +1,13 @@
+#include "MyForm.h"
+
+using namespace ProjectLR44;
+
+[STAThread]
+int main(array<String^>^ arg) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    ProjectLR44::MyForm form;
+    Application::Run(% form);
+    return 0;
+}
